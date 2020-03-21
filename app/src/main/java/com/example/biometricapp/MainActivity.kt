67abf2d9fc92b70419b5b3c3dc68b3ff626ede2c
ManavShakya <manavshakya8@gmail.com>
2020-03-21 13:59:00 +0530

    Made some changes

--- app/src/main/java/com/example/biometricapp/MainActivity.kt
+++ app/src/main/java/com/example/biometricapp/MainActivity.kt
@@ -1,81 +1,117 @@
 package com.example.biometricapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
-import java.util.concurrent.Executor
+import java.nio.charset.Charset
 
 class MainActivity : AppCompatActivity() {
-    
-    private lateinit var executor: Executor
-    private lateinit var biometricManager: BiometricManager
+
+    private lateinit var textInputView: AppCompatEditText
+    private lateinit var textOutputView: AppCompatTextView
     private lateinit var biometricPrompt: BiometricPrompt
-    private lateinit var mainLayout: View
-    
+    private lateinit var promptInfo: BiometricPrompt.PromptInfo
+    private var readyToEncrypt: Boolean = false
+    private lateinit var cryptographyManager: CryptographyManager
+    private lateinit var secretKeyName: String
+    private lateinit var ciphertext:ByteArray
+    private lateinit var initializationVector: ByteArray
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        mainLayout = findViewById(R.id.mainLayout)
-        mainLayout.visibility = View.GONE
-        executor = ContextCompat.getMainExecutor(this)
-        biometricManager = BiometricManager.from(this)
-        checkHardware(biometricManager)
+        cryptographyManager = CryptographyManager()
+        // e.g. secretKeyName = "biometric_sample_encryption_key"
+        secretKeyName = "confidential"
+        biometricPrompt = createBiometricPrompt()
+        promptInfo = createPromptInfo()
 
+        textInputView = findViewById(R.id.input_view)
+        textOutputView = findViewById(R.id.output_view)
+        findViewById<Button>(R.id.encrypt_button).setOnClickListener { authenticateToEncrypt() }
+        findViewById<Button>(R.id.decrypt_button).setOnClickListener { authenticateToDecrypt() }
     }
 
-    /**
-     * function to check if the user’s device supports biometric features before you perform any authentication.
-     */
-    private fun checkHardware(biometricManager: BiometricManager) {
-        when (biometricManager.canAuthenticate()) {
+    private fun createBiometricPrompt(): BiometricPrompt {
+        val executor = ContextCompat.getMainExecutor(this)
 
-            BiometricManager.BIOMETRIC_SUCCESS -> authenticateUser(executor)
-            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> authenticateUser(executor)
-            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> Toast
-                .makeText(this, "Biometric features are currently unavailable", Toast.LENGTH_LONG).show()
-            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> Toast
-                .makeText(this, "Please setup biometric credentials and try again", Toast.LENGTH_LONG).show()
+        val callback = object : BiometricPrompt.AuthenticationCallback() {
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                Log.d(TAG, "$errorCode :: $errString")
+            }
+
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                Log.d(TAG, "Authentication failed for an unknown reason")
+            }
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                Log.d(TAG, "Authentication was successful")
+                processData(result.cryptoObject)
+            }
+        }
+
+        //The API requires the client/Activity context for displaying the prompt view
+        val biometricPrompt = BiometricPrompt(this, executor, callback)
+        return biometricPrompt
+    }
+
+    private fun createPromptInfo(): BiometricPrompt.PromptInfo {
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Sign in") // e.g. "Sign in"
+            .setSubtitle("Biometric for My App") // e.g. "Biometric for My App"
+            .setDescription("Confirm biometric to continue") // e.g. "Confirm biometric to continue"
+            .setConfirmationRequired(true)
+            //.setNegativeButtonText(getString(R.string.prompt_info_use_app_password)) // e.g. "Use Account Password"
+            .setDeviceCredentialAllowed(true) // Allow PIN/pattern/password authentication.
+            // Also note that setDeviceCredentialAllowed and setNegativeButtonText are
+            // incompatible so that if you uncomment one you must comment out the other
+            .build()
+        return promptInfo
+    }
+
+    private fun authenticateToEncrypt() {
+        readyToEncrypt = true
+        if (BiometricManager.from(applicationContext).canAuthenticate() == BiometricManager
+                .BIOMETRIC_SUCCESS) {
+            val cipher = cryptographyManager.getInitializedCipherForEncryption(secretKeyName)
+            biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
         }
     }
 
-    private fun authenticateUser(executor: Executor){
-
-        biometricPrompt = BiometricPrompt(this, executor,
-            object : BiometricPrompt.AuthenticationCallback() {
-
-                override fun onAuthenticationSucceeded(
-                    result: BiometricPrompt.AuthenticationResult
-                ) {
-                    super.onAuthenticationSucceeded(result)
-                    mainLayout.visibility = View.VISIBLE
-                }
-
-                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
-                    super.onAuthenticationError(errorCode, errString)
-                    Toast.makeText(applicationContext, errString, Toast.LENGTH_LONG).show()
-                }
-
-                override fun onAuthenticationFailed() {
-                    super.onAuthenticationFailed()
-                    Toast.makeText(applicationContext, "Authentication failed", Toast.LENGTH_SHORT).show()
-                }
-            })
-
-        val promptInfo = BiometricPrompt.PromptInfo.Builder()
-            .setTitle("Authentication Required")
-            .setSubtitle("Log in using Biometric")
-            .setDescription("Touch your fingerprint sensor to authenticate")
-            .setDeviceCredentialAllowed(true)
-            .setConfirmationRequired(true)
-            .build()
-
-        biometricPrompt.authenticate(promptInfo)
+    private fun authenticateToDecrypt() {
+        readyToEncrypt = false
+        if (BiometricManager.from(applicationContext).canAuthenticate() == BiometricManager
+                .BIOMETRIC_SUCCESS) {
+            val cipher = cryptographyManager.getInitializedCipherForDecryption(secretKeyName,initializationVector)
+            biometricPrompt.authenticate(promptInfo, BiometricPrompt.CryptoObject(cipher))
+        }
 
     }
 
+    private fun processData(cryptoObject: BiometricPrompt.CryptoObject?) {
+        val data = if (readyToEncrypt) {
+            val text = textInputView.text.toString()
+            val encryptedData = cryptographyManager.encryptData(text, cryptoObject?.cipher!!)
+            ciphertext = encryptedData.ciphertext
+            initializationVector = encryptedData.initializationVector
 
+            String(ciphertext, Charset.forName("UTF-8"))
+        } else {
+            cryptographyManager.decryptData(ciphertext, cryptoObject?.cipher!!)
+        }
+        textOutputView.text = data
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+    }
 }
